25b8366f8fe4825bfd932b2e1eebd022b42b889b
Ruby <88955684+Ruby11692046@users.noreply.github.com>
2026-09-13 23:14:58 +0900

    スマホ版のZEN Portalでも動作するようにパターンを追加

--- js/services/TranscriptParser.js
+++ js/services/TranscriptParser.js
@@ -1,10 +1,18 @@
 const TEACHER_NAME_PATTERN = /^[一-龠々ぁ-んァ-ヶーA-Za-z]+[\s・][一-龠々ぁ-んァ-ヶーA-Za-z]+$/;
 
+const MOBILE_LINE_PATTERN = /^(?:※\t)?(.+?)\[[^\]]+\]\d{4}\dQ$/;
+
 export function parseCourseNames(rawText) {
     const lines = rawText.split('\n');
     const courseNames = new Set();
 
     for (const line of lines) {
+        const mobileMatch = line.match(MOBILE_LINE_PATTERN);
+        if (mobileMatch) {
+            courseNames.add(mobileMatch[1].trim());
+            continue;
+        }
+
         const cells = line.split('\t').map(cell => cell.trim());
         const nonEmptyCells = cells.filter(cell => cell !== '');
 
